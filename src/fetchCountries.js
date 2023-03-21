c960e45154e22eaf57b565e4a912826608bf318c
IhorKiy <kiyanichenko@ukr.net>
2023-03-21 17:30:19 +0200

    3.21

---
+++ src/fetchCountries.js
@@ -0,0 +1,28 @@
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = '?fields=name,flags,capital,population,languages';
+
+export async function fetchCountries(name) {
+  return fetch(`${BASE_URL}${name}${searchParams}`)
+    .then(response => {
+      if (!response.ok) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        return;
+      } else return response.json();
+    })
+
+    .then(response => {
+      console.log(response);
+      if (response.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name'
+        );
+        return;
+      }
+      return response;
+    });
+  // .catch(error => {
+  //   console.log(error);
+  // });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const ref = {
+  input: document.querySelector('#search-box'),
+  nameCountry: document.querySelector('.country-list'),
+  infoCountry: document.querySelector('.country-info'),
+};
+
+ref.input.addEventListener(
+  'input',
+  debounce(render, [(wait = DEBOUNCE_DELAY)], [(options = {})])
+);
+
+function render(input) {
+  const inpt = input.target.value.trim();
+
+  if (inpt === '') {
+    ref.nameCountry.innerHTML = '';
+    ref.infoCountry.innerHTML = '';
+    return;
+  }
+  fetchCountries(inpt).then(countries => {
+    if (countries.length > 1) {
+      let markup = '';
+      markup = countries.map(
+        ({ name: { official }, flags: { svg } }) =>
+          `<li> <img width ="50" height = "30" src='${svg}' alt='${official}'>    ${official}   </li>`
+      );
+      ref.nameCountry.innerHTML = markup;
+      ref.infoCountry.innerHTML = '';
+    } else {
+      console.log(countries);
+
+      const { name, flags, capital, population, languages } = countries[0];
+      const languagesArray = Object.values(languages).join(', ');
+
+      markup = `<img width ="80" height = "50" src='${flags.svg}' alt='${name.official}'><p>${name.official}</p><p>${capital}</p><p>${population}</p><p>${languagesArray}</p>`;
+      ref.nameCountry.innerHTML = '';
+      ref.infoCountry.innerHTML = markup;
+    }
+  });
+}
